7474fb1bd849bd8f83c178027edc0dce2d968543
kasra <82941023+kasra28@users.noreply.github.com>
2023-11-25 09:55:32 +0330

    Update note.js

--- note.js
+++ note.js
@@ -12,3 +12,17 @@ function LightTheme(){
     document.querySelector('.footer').classList.add('footer-light')
     document.querySelector('.mail').classList.add('mail-tar')
 }
+function DarkTheme(){
+    document.querySelector('.target').classList.remove('body-light');
+    document.querySelector('.header').classList.remove('header-light');
+    document.querySelectorAll('.header-links').forEach(headerLink => {
+        headerLink.classList.remove('header-links-light');
+    });
+    document.querySelector('.button').classList.remove('button-light');
+    document.querySelectorAll('.headerPar').forEach(headerPar => {
+        headerPar.classList.remove('header-par-light')
+    });
+    document.querySelector('.main').classList.remove('main-light')
+    document.querySelector('.footer').classList.remove('footer-light')
+    document.querySelector('.mail').classList.remove('mail-tar')
+}
