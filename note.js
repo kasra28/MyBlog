9d316c6b92f0d42dede946b25e5b97428a58af8f
kasra <82941023+kasra28@users.noreply.github.com>
2023-12-02 09:29:00 +0330

    Update note.js

--- note.js
+++ note.js
@@ -1,28 +1,14 @@
 function LightTheme(){
-    document.querySelector('.target').classList.add('body-light');
-    document.querySelector('.header').classList.add('header-light');
+    document.querySelector('.target').classList.toggle('body-light');
+    document.querySelector('.header').classList.toggle('header-light');
     document.querySelectorAll('.header-links').forEach(headerLink => {
-        headerLink.classList.add('header-links-light');
+        headerLink.classList.toggle('header-links-light');
     });
-    document.querySelector('.button').classList.add('button-light');
+    document.querySelector('.button').classList.toggle('button-light');
     document.querySelectorAll('.headerPar').forEach(headerPar => {
-        headerPar.classList.add('header-par-light')
+        headerPar.classList.toggle('header-par-light')
     });
-    document.querySelector('.main').classList.add('main-light')
-    document.querySelector('.footer').classList.add('footer-light')
-    document.querySelector('.mail').classList.add('mail-tar')
-}
-function DarkTheme(){
-    document.querySelector('.target').classList.remove('body-light');
-    document.querySelector('.header').classList.remove('header-light');
-    document.querySelectorAll('.header-links').forEach(headerLink => {
-        headerLink.classList.remove('header-links-light');
-    });
-    document.querySelector('.button').classList.remove('button-light');
-    document.querySelectorAll('.headerPar').forEach(headerPar => {
-        headerPar.classList.remove('header-par-light')
-    });
-    document.querySelector('.main').classList.remove('main-light')
-    document.querySelector('.footer').classList.remove('footer-light')
-    document.querySelector('.mail').classList.remove('mail-tar')
+    document.querySelector('.main').classList.toggle('main-light')
+    document.querySelector('.footer').classList.toggle('footer-light')
+    document.querySelector('.mail').classList.toggle('mail-tar')
 }
